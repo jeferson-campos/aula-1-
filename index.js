5d6a2b3f775ab5a56c52ae766077ff63b5e9ee74
jeferson-campos <jefersoncampos5296@gmail.com>
2024-10-14 12:14:14 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+// alert('ola mundo ')
+
+    // var numero = 10 
+    // alert('10')
+  
+    //    let numero = 10 
+    // alert('10')
+
+    //    const numero = 10 ;
+    // alert('10')
+
+// let num1 = parseFloat(prompt("Digite o primeiro número:"));
+// let num2 = parseFloat(prompt("Digite o segundo número:"));
+
+
+// let soma = num1 + num2;
+// alert("Soma: " + soma);
+
